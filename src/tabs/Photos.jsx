4e9c929ac4f721236365b035cdf18333368f7575
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-05-26 11:26:37 +0300

    task-01

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,19 @@
 // import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { Form, Text } from 'components';
+import { useState } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState("");
+
+const onHandleSubmit = (searchQuery) => {
+  console.log(searchQuery);
+setQuery(searchQuery);
+}
+
   return (
     <>
       <Text textAlign="center">Let`s begin search 🔎</Text>
+    <Form onSubmit={onHandleSubmit}/>
     </>
   );
 };
